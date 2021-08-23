b978ca436b9dbee0e9fc90a4243f5dff7058d9b1
Ewgenij Belzmann <ewgbel@gmail.com>
2021-08-23 23:20:24 +0200

    Extends Order entity with properties for order time and priority status (derived property)

--- src/main/java/de/belzmann/donut/model/Order.java
+++ src/main/java/de/belzmann/donut/model/Order.java
@@ -1,9 +1,19 @@
 package de.belzmann.donut.model;
 
+import org.hibernate.annotations.Formula;
+
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 public class Order {
+
+    /**
+     * The cutoff for premium customers. Customers with id
+     * lower than the cutoff are considered premium and their orders
+     * are processed before the regular customers.
+     */
+    public static final int PREMIUM_CLIENT_CUTOFF = 1000;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,12 +25,24 @@ public class Order {
     @Column(nullable = false)
     private int donutQuantity;
 
+    @Column(nullable = false)
+    private Timestamp orderTime;
+
+    /**
+     * Determines whether the order is for a premium customer.
+     * This is info is derived from the customer number and
+     * is not stored in the database.
+     */
+    @Formula("clientId < " + PREMIUM_CLIENT_CUTOFF)
+    private boolean isPriority;
+
     public Order() {
     }
 
-    public Order(int clientId, int donutQuantity) {
+    public Order(int clientId, int donutQuantity, Timestamp orderTime) {
         this.clientId = clientId;
         this.donutQuantity = donutQuantity;
+        this.orderTime = orderTime;
     }
 
     public int getOrderId() {
@@ -38,6 +60,7 @@ public class Order {
 
     public Order setClientId(int clientId) {
         this.clientId = clientId;
+        this.isPriority = (clientId < PREMIUM_CLIENT_CUTOFF);
         return this;
     }
 
@@ -48,5 +71,18 @@ public class Order {
     public Order setDonutQuantity(int donutQuantity) {
         this.donutQuantity = donutQuantity;
         return this;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public Order setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+        return this;
+    }
+
+    public boolean isPriority() {
+        return isPriority;
     }
 }
